617503e906e145593cfb9200ab403ded5580a3ec
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-16 23:24:06 +0600

    flex

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1e40af',
+        secondary: '#000000',
+
+      },
+    
+    },
   },
   plugins: [],
 }
